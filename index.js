12747047d7f04f4b23fddca1a928800932c57f16
Georgemuchiri <georgw.muchiri@student.moringaschool.com>
2024-07-04 08:10:48 +0300

    Add my  files

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somenumber) {
+    const headquaters = 42;
+    return Math.abs(somenumber-headquaters);
+}
+
+function distanceFromHqInFeet(somenumber) {
+    const headquarters = 42;
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(somenumber - headquarters);
+    return distanceInBlocks * feetPerBlock;
+}
+
+// function distanceTravelledInFeet(somenumber){
+//     const headquarters=42;
+//     const feetPerBlock=1320;
+//     const distanceTravelledInFeet=2640;
+//     return distanceInBlocks*feetPerBlock;
+// }
+
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(start - destination);
+    return distanceInBlocks * feetPerBlock;
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
